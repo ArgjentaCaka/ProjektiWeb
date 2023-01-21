57e1f216e9171e9c85c96bb6e5cf03dfa4804f6d
38346 <38346@DESKTOP-QK8141T>
2023-01-21 11:30:50 +0100

    Commit

--- recipes.php
+++ recipes.php
@@ -1,3 +1,50 @@
+<?php
+include("includes/db.php");
+
+
+if(isset($_POST['addRecipe'])){
+ 
+  $name = $_FILES['file']['name'];
+  $target_dir = "images/";
+  $target_file = $target_dir . basename($_FILES["file"]["name"]);
+  $err = "";
+  $success = "";
+
+  $recipeName = $_POST['recipeName'];
+  $readyIn = $_POST['readyIn'];
+  $ingredients = $_POST['ingredients'];
+  $notes = $_POST['notes'];
+  $isHealthy = $_POST['isHealthy'];
+  $addedBy = $_SESSION['username'];
+
+  if(empty($recipeName) || empty($readyIn) || empty($ingredients) || empty($notes)){
+    $err = "Information missing";
+  }
+
+  // Select file type
+  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+  // Valid file extensions
+  $extensions_arr = array("jpg","jpeg","png","gif");
+
+  // Check extension
+  if( in_array($imageFileType,$extensions_arr) && empty($err)){
+     // Upload file
+     if(move_uploaded_file($_FILES['file']['tmp_name'],$target_dir.$name)){
+      $insert = "INSERT into recipe(name,ingredients,directions,readyIn,addedBy,image,isHealthy) 
+      values('$recipeName','$ingredients','$notes','$readyIn','$addedBy','$name','$isHealthy')";
+      if(mysqli_query($link, $insert)){
+        $success = "Recipe added";
+      }
+      else{
+        echo 'Error: '.mysqli_error($link);
+      }
+     }
+
+  }
+ 
+}
+?>
 <!DOCTYPE HTML>
 <html>
 
@@ -17,21 +64,42 @@
     <div id="header">
       <div id="logo">
         <div id="logo_text">
-          <h1><a href="index.html"><span class="logo_colour"><font color="#000000">Yummy</font>Bite</span></a></h1>
+          <h1><a href="index.php"><span class="logo_colour"><font color="#000000">Yummy</font>Bite</span></a></h1>
           <h2>The foodie place</h2>
         </div>
       </div>
-     </div>
+	<?php
+	include 'nav.php';
+	?>
+    </div>
     <div id="site_content">
 <div id="content">
-   
+    <?php if(!empty($_SESSION['username'])){?>
 <div class="main_content" style="display:flex; flex-direction: row;">
 <div style="width:50%;">
 <h4>My Recipes</h4><br />
 <ul>
+<?php
+    $addedBy = $_SESSION['username'];
+    $sql = "SELECT * FROM recipe where addedBy='".$addedBy."'";
+    $result = $link->query($sql);
 
-     
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
       
+      ?>
+      <li>
+          <a class="read_more" href="recipe.php?id=<?php echo $row['id'];?>"><i class="fa fa-long-arrow-right"></i></a>
+          <?php echo $row['name'];?>
+      </li>
+      <?php
+      }
+      } else {
+      echo "0 results";
+    }
+    $link->close();
+    ?>
     </ul>
 </div>
 	      <div class="sidebar" style="width:50%;">
@@ -68,10 +136,15 @@
         </form>
       </div>
   </div>
+  <?php
+  }else{
+      echo '<br /><p>You need to log in to use this feature</p></br />';
+  }
+  ?>
         </div>
       </div>
    
-   <div id="footer"> Krijuar nga :&nbsp; Argjenta Caka & Elza Krasniqi &nbsp;<p><a href="www.facebook.com">CEO</a></p></div>
+   <div id="footer"> Krijuar nga :&nbsp; Albesa Halili & Fitore Ramadani &nbsp;<p><a href="www.facebook.com">CEO</a></p></div>
   </div>
 </body>
 </html>
